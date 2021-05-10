6617dde1f948e1cbe073f8385c1248b872746994
Henry Del Cristo Bravo Almanza <hbravoal@gmail.com>
2021-05-10 02:37:34 -0500

    Add config

--- Weelo/src/Weelo.Application/Features/PropertiesImage/Commands/Create/CreatePropertyImageCommand.cs
+++ Weelo/src/Weelo.Application/Features/PropertiesImage/Commands/Create/CreatePropertyImageCommand.cs
@@ -3,6 +3,7 @@ using MediatR;
 using System.Threading;
 using System.Threading.Tasks;
 using Weelo.Application.Interfaces.Repositories;
+using Weelo.Application.Services;
 using Weelo.Application.Wrappers;
 using Weelo.Domain.Entities;
 
@@ -21,11 +22,15 @@ namespace Weelo.Application.Features.PropertiesImage.Commands.Create
         private readonly IPropertyImageRepositoryAsync _repository;
         private readonly IMapper _mapper;
         public static Microsoft.Extensions.Hosting.IHostEnvironment _environment;
-        public CreatePropertyImageCommandHandler(IPropertyImageRepositoryAsync repository, IMapper mapper, Microsoft.Extensions.Hosting.IHostEnvironment environment)
+        private readonly IDataProtectionHelper _dataProtectionHelper;
+
+        public CreatePropertyImageCommandHandler(IPropertyImageRepositoryAsync repository, IMapper mapper,
+            Microsoft.Extensions.Hosting.IHostEnvironment environment, Services.IDataProtectionHelper dataProtectionHelper)
         {
             _repository = repository;
             _mapper = mapper;
             _environment = environment;
+            _dataProtectionHelper = dataProtectionHelper;
         }
 
         public async Task<Response<int>> Handle(CreatePropertyImageCommand request, CancellationToken cancellationToken)
@@ -34,20 +39,25 @@ namespace Weelo.Application.Features.PropertiesImage.Commands.Create
             //TODO: Optimizing GetEntryAssembly -> Why? Visual's Daemon don't work for autoc for usings.
                 try
                 {
-                string path = System.IO.Path.Combine(System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly().Location), "uplods");
+                string path = System.IO.Path.Combine(System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly().Location), "uploads");
                 if (!System.IO.Directory.Exists(path))
                     {
                         System.IO.Directory.CreateDirectory(path);
                     }
-
-                string op = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly().Location)+ "\\uploads\\" + request.File.FileName.ToString();
                 
+                var extension = System.IO.Path.GetExtension(request.File.FileName);
+                string imageName = request.File.FileName + extension;
+                string imageContainer = _dataProtectionHelper.Encrypt(imageName);
+                string pathImage = System.IO.Path.Combine(path, imageContainer);
+                pathImage += extension;
 
-                using (System.IO.FileStream filestream = System.IO.File.Create(path+request.File.FileName))
+
+
+                using (System.IO.FileStream filestream = System.IO.File.Create(pathImage))
                     {
                         request.File.CopyTo(filestream);
                         filestream.Flush();
-                        string filepath= "\\uploads\\" + request.File.FileName;
+                    string filepath = imageContainer;
                         var result = _mapper.Map<PropertyImage>(request);
                     
                             result.File = filepath;
